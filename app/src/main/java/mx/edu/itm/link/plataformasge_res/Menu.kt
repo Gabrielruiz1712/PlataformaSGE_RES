e74b04cf207174998174f8d41f14844ca2b79189
Alblerto Oseguera <luis-demo@hotmail.com>
2021-06-19 15:42:18 -0500

    Se agrego botones al menu principal

--- app/src/main/java/mx/edu/itm/link/plataformasge_res/Menu.kt
+++ app/src/main/java/mx/edu/itm/link/plataformasge_res/Menu.kt
@@ -1,13 +1,26 @@
 package mx.edu.itm.link.plataformasge_res
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mx.edu.itm.link.plataformasge_res.databinding.ActivityMenuBinding
+import mx.edu.itm.link.plataformasge_res.databinding.ActivityMenuResidencias2Binding
+
+
 
 class Menu : AppCompatActivity() {
+    private lateinit var binding: ActivityMenuBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val alumno = intent.getStringExtra("ALUMNO")
+
+        binding.btnResi.setOnClickListener {
+            val intent = Intent(this, MenuResidencias::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/mx/edu/itm/link/plataformasge_res/MenuResidencias.kt
@@ -0,0 +1,11 @@
+package mx.edu.itm.link.plataformasge_res
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuResidencias : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_residencias2)
+    }
+}
